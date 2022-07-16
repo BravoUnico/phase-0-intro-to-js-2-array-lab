42019ab2078a51cb286f5e4553d33331a906df31
Alejandro <sancha0701@gmail.com>
2022-07-16 10:17:03 -0400

    commit tree 78442ccee978b7ca868365832bfb1197178c25fc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+
+const cats = ["Milo","Otis","Garfield",];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Milo")
+}function appendCat(name) {
+    const newcats = [...cats, "Broom"]; //  ✓ appends a cat to the cats array and returns a new array, leaving the cats array unchanged
+    return newcats  
+}
+
+function prependCat(name) {
+    const newcats = ["Arnold", ...cats]; // ✓ prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
+    return newcats
+}
+function removeLastCat(name) {
+    const copycats = cats.slice(0, cats.length - 1) //  removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+    return copycats  
+}
+
+function removeFirstCat(name) {
+    const copycats = cats.slice(1); // ✓ removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
+    return copycats
+}
